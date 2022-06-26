097f2718b6b409b0df9c1237341283bbdc713170
luchoevan <lucho.evangelisti@hotmail.com>
2022-06-26 06:54:58 -0300

    add initial files

--- 04-JS-III/homework/homework.js
+++ 04-JS-III/homework/homework.js
@@ -30,7 +30,9 @@ function agregarItemAlFinalDelArray(array, elemento) {
   // Añade el "elemento" al final del array
   // y devuelve el array
   // Tu código:
+
 }
+
 
 
 function agregarItemAlComienzoDelArray(array, elemento) {
@@ -54,6 +56,11 @@ function arrayContiene(array, elemento) {
   // Comprueba si el elemento existe dentro de "array"
   // Devuelve "true" si está, o "false" si no está
   // Tu código:
+  //for (var i = 0; i < array.length; i++) { // [1 , 2 , 3 , 4]
+  // if(array [i] === elemento) return true;
+  // }
+  // return false
+  return array.includes(elemento);
 }
 
 
@@ -61,13 +68,25 @@ function agregarNumeros(numeros) {
   // "numeros" debe ser un arreglo de enteros (int/integers)
   // Suma todos los enteros y devuelve el valor
   // Tu código:
+  var acc = 0;
+  for (var i = 0; i < numeros.length; i++) {
+    acc += numeros[i];  
+  }
 }
+
 
 
 function promedioResultadosTest(resultadosTest) {
   // "resultadosTest" debe ser una matriz de enteros (int/integers)
   // Itera (en un bucle) los elementos del array, calcula y devuelve el promedio de puntajes
   // Tu código:
+  var sumaTotal = O;
+  var numeroTest = resultadosTest.length;
+  for (var i = 0; i < resultadosTest.Test.length; i++){
+    sumaTotal += resultadoTest[i]
+  }
+  var promedio = sumaTotal / numeroTest;
+  return promedio; 
 }
 
 
@@ -75,6 +94,13 @@ function numeroMasGrande(numeros) {
   // "numeros" debe ser una matriz de enteros (int/integers)
   // Devuelve el número más grande
   // Tu código:
+  var acc = 0;
+  for (let i = 0; i < numeros.length; i++) {
+    if (numeros[i] > acc) {
+      acc = numeros[i];
+    }
+  }
+  return acc;
 }
 
 
@@ -82,13 +108,27 @@ function multiplicarArgumentos() {
   // Usa la palabra clave `arguments` para multiplicar todos los argumentos y devolver el producto
   // Si no se pasan argumentos devuelve 0. Si se pasa un argumento, simplemente devuélvelo
   // Escribe tu código aquí:
+  if (arguments.length === o) return 0;  // arguments = []
+  else {
+    for (let i = 0; i < arguments.length; i++) {
+      producto *= arguments[i]
+    }
+  }
+  return producto;
 }
+
 
 
 function cuentoElementos(arreglo){
   //Realiza una función que retorne la cantidad de los elementos del arreglo cuyo valor es mayor a 18.
   //Escribe tu código aquí
-
+  var acc = 0; 
+  for (let i = 0; i < arreglo,length; i++) {
+    if (arreglo[i] > 18) {
+      acc = acc + 1;
+    }
+  }
+  return acc; 
 }
 
 
@@ -97,7 +137,9 @@ function diaDeLaSemana(numeroDeDia) {
   //Realiza una función que dado el número del día de la semana, retorne: Es fin de semana
   //si el día corresponde a Sábado o Domingo y “Es dia Laboral” en caso contrario. 
   //Escribe tu código aquí   
-  
+  if (numeroDeDia === 1 || numeroDeDia === 7)
+  return "Es fin de semana";
+  return "Es dia Laboral";
 } 
 
 
@@ -105,7 +147,9 @@ function empiezaConNueve(n) {
   //Desarrolle una función que recibe como parámetro un número entero n. Debe retornar true si el entero 
   //inicia con 9 y false en otro caso.
   //Escribe tu código aquí
-  
+  let numero = n.toString ();
+  let primeNum = numero.split("");
+  if(primeNum[0])
 }
 
 
